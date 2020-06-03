10ba340ee8ec71b2a4bc93609baf977a669a720d
zhyingkun <1464959348@qq.com>
2020-06-03 22:55:42 +0800

    Update mail source for modules

--- src/mail/ngx_mail_module_modules.c
+++ src/mail/ngx_mail_module_modules.c
@@ -5,6 +5,9 @@
 extern ngx_module_t  ngx_mail_module;
 extern ngx_module_t  ngx_mail_core_module;
 extern ngx_module_t  ngx_mail_ssl_module;
+extern ngx_module_t  ngx_mail_pop3_module;
+extern ngx_module_t  ngx_mail_imap_module;
+extern ngx_module_t  ngx_mail_smtp_module;
 extern ngx_module_t  ngx_mail_auth_http_module;
 extern ngx_module_t  ngx_mail_proxy_module;
 
@@ -12,6 +15,9 @@ ngx_module_t *ngx_modules[] = {
     &ngx_mail_module,
     &ngx_mail_core_module,
     &ngx_mail_ssl_module,
+    &ngx_mail_pop3_module,
+    &ngx_mail_imap_module,
+    &ngx_mail_smtp_module,
     &ngx_mail_auth_http_module,
     &ngx_mail_proxy_module,
     NULL
@@ -21,6 +27,9 @@ char *ngx_module_names[] = {
     "ngx_mail_module",
     "ngx_mail_core_module",
     "ngx_mail_ssl_module",
+    "ngx_mail_pop3_module",
+    "ngx_mail_imap_module",
+    "ngx_mail_smtp_module",
     "ngx_mail_auth_http_module",
     "ngx_mail_proxy_module",
     NULL
