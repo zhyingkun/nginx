242ccc44ab2bb9e751f79968e56a9611b1ea01d0
zhyingkun <1464959348@qq.com>
2020-06-03 22:55:56 +0800

    Format source files

--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -4,53 +4,45 @@
  * Copyright (C) Nginx, Inc.
  */
 
-
 #ifndef _NGX_LIST_H_INCLUDED_
 #define _NGX_LIST_H_INCLUDED_
-
 
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
-typedef struct ngx_list_part_s  ngx_list_part_t;
+typedef struct ngx_list_part_s ngx_list_part_t;
 
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
-    ngx_list_part_t  *next;
+  void* elts;
+  ngx_uint_t nelts;
+  ngx_list_part_t* next;
 };
 
-
 typedef struct {
-    ngx_list_part_t  *last;
-    ngx_list_part_t   part;
-    size_t            size;
-    ngx_uint_t        nalloc;
-    ngx_pool_t       *pool;
+  ngx_list_part_t* last;
+  ngx_list_part_t part;
+  size_t size;
+  ngx_uint_t nalloc;
+  ngx_pool_t* pool;
 } ngx_list_t;
 
+ngx_list_t* ngx_list_create(ngx_pool_t* pool, ngx_uint_t n, size_t size);
 
-ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
+static ngx_inline ngx_int_t ngx_list_init(ngx_list_t* list, ngx_pool_t* pool, ngx_uint_t n, size_t size) {
+  list->part.elts = ngx_palloc(pool, n * size);
+  if (list->part.elts == NULL) {
+    return NGX_ERROR;
+  }
 
-static ngx_inline ngx_int_t
-ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
-{
-    list->part.elts = ngx_palloc(pool, n * size);
-    if (list->part.elts == NULL) {
-        return NGX_ERROR;
-    }
+  list->part.nelts = 0;
+  list->part.next = NULL;
+  list->last = &list->part;
+  list->size = size;
+  list->nalloc = n;
+  list->pool = pool;
 
-    list->part.nelts = 0;
-    list->part.next = NULL;
-    list->last = &list->part;
-    list->size = size;
-    list->nalloc = n;
-    list->pool = pool;
-
-    return NGX_OK;
+  return NGX_OK;
 }
-
 
 /*
  *
@@ -76,8 +68,6 @@ ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
  *  }
  */
 
-
-void *ngx_list_push(ngx_list_t *list);
-
+void* ngx_list_push(ngx_list_t* list);
 
 #endif /* _NGX_LIST_H_INCLUDED_ */
